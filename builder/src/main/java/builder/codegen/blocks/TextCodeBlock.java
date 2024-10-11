5d6d2f8265359ac793b9a98ade62b2785f402c83
Area52_Nader <nader@area52.ca>
2024-10-11 11:20:24 -0300

    Added "Text is a Variable?" in the display-only text element, this will let user choose to output text with or without quotation marks "" if they have the text already predefined, in my case text is #define VAR "text1" so in GSbuilder "text = VAR" and "Text is a Variable = true"

--- builder/src/main/java/builder/codegen/blocks/TextCodeBlock.java
+++ builder/src/main/java/builder/codegen/blocks/TextCodeBlock.java
@@ -49,19 +49,19 @@ import builder.models.WidgetModel;
 public final class TextCodeBlock implements CodeBlock {
 
   /** The Constants for TEMPLATES. */
-  private final static String ALIGN_TEMPLATE         = "<TEXTALIGN>";
-  private final static String ELEMENTREF_TEMPLATE    = "<ELEMENT_REF>";
-  private final static String FILL_EN_TEMPLATE       = "<FILL_EN>";
-  private final static String FRAME_EN_TEMPLATE      = "<FRAME_EN>";
-  private final static String TEXT_TEMPLATE          = "<TEXT>";
-  private final static String TEXTCOLOR_TEMPLATE     = "<TEXT_COLOR>";
-  private final static String TEXTMARGIN_TEMPLATE     = "<TEXT_MARGIN>";
-  private final static String TEXT_UPDATE_TEMPLATE   = "<TEXT_UPDATE>";
-  private final static String TEXTUTF8_TEMPLATE      = "<TEXT_UTF8>";
-  private final static String COLOR_TEMPLATE         = "<COLOR>";
+  private final static String ALIGN_TEMPLATE = "<TEXTALIGN>";
+  private final static String ELEMENTREF_TEMPLATE = "<ELEMENT_REF>";
+  private final static String FILL_EN_TEMPLATE = "<FILL_EN>";
+  private final static String FRAME_EN_TEMPLATE = "<FRAME_EN>";
+  private final static String TEXT_TEMPLATE = "<TEXT>";
+  private final static String TEXTCOLOR_TEMPLATE = "<TEXT_COLOR>";
+  private final static String TEXTMARGIN_TEMPLATE = "<TEXT_MARGIN>";
+  private final static String TEXT_UPDATE_TEMPLATE = "<TEXT_UPDATE>";
+  private final static String TEXTUTF8_TEMPLATE = "<TEXT_UTF8>";
+  private final static String COLOR_TEMPLATE = "<COLOR>";
 
   /** The Constants for MACROS */
-  private final static String TEXT_MACRO             = "TEXT";
+  private final static String TEXT_MACRO = "TEXT";
 
   /**
    * Instantiates a new check box code block.
@@ -74,25 +74,26 @@ public final class TextCodeBlock implements CodeBlock {
    * our input string builder object.
    *
    * @param cg
-   *          the cg points to our CodeGenerator object that 
-   *          is the controller for code generation.
+   *                 the cg points to our CodeGenerator object that
+   *                 is the controller for code generation.
    * @param tm
-   *          the tm is the TemplateManager
+   *                 the tm is the TemplateManager
    * @param sBd
-   *          the sBd is the processed code
+   *                 the sBd is the processed code
    * @param pageEnum
-   *          the page enum
+   *                 the page enum
    * @param wm
-   *          the wm is the widget model to use for code generation
+   *                 the wm is the widget model to use for code generation
    * @return the <code>string builder</code> object
    */
-  static public StringBuilder process(CodeGenerator cg, TemplateManager tm, StringBuilder sBd, String pageEnum, WidgetModel wm) {
-    TextModel m = (TextModel)wm;
+  static public StringBuilder process(CodeGenerator cg, TemplateManager tm, StringBuilder sBd, String pageEnum,
+      WidgetModel wm) {
+    TextModel m = (TextModel) wm;
     String templateName = null;
     List<String> template = null;
     List<String> outputLines = null;
     Map<String, String> map = m.getMappedProperties(pageEnum);
-    
+
     // now output creation API
     int ts = m.getTextStorage();
     if (ts > 0) {
@@ -101,18 +102,25 @@ public final class TextCodeBlock implements CodeBlock {
       templateName = TEXT_TEMPLATE;
     }
     template = tm.loadTemplate(templateName);
-    
+
     String fontName = m.getFontDisplayName();
     FontTFT font = FontFactory.getInstance().getFont(fontName);
-    /* we can't use standard mapping of TXT-201 since we may need
+    /*
+     * we can't use standard mapping of TXT-201 since we may need
      * to handle converting utf8 to hex characters and deal with builtin
      * (classic) character sets that be not be in display 32-126 ascii range.
      */
-    map.put(TEXT_MACRO, CodeUtils.createLiteral(font, "\"", m.getText()));
-
+    String text = m.getText();
+    
+    if (m.isStringEnabled() && text.length() >= 2) {
+      // Remove first and last character
+      map.put(TEXT_MACRO, m.getText());
+    } else {
+      map.put(TEXT_MACRO, CodeUtils.createLiteral(font, "\"", m.getText()));
+    }
     outputLines = tm.expandMacros(template, map);
     tm.codeWriter(sBd, outputLines);
-    
+
     // now deal with any extra switches
     String strAlign = m.getAlignment();
     if (!strAlign.equals(FontTFT.ALIGN_LEFT)) {
@@ -141,13 +149,13 @@ public final class TextCodeBlock implements CodeBlock {
       tm.codeWriter(sBd, outputLines);
     }
     if ((!m.getFrameColor().equals(TextModel.DEF_FRAME_COLOR)) ||
-        (!m.getFillColor().equals(TextModel.DEF_FILL_COLOR))  || 
+        (!m.getFillColor().equals(TextModel.DEF_FILL_COLOR)) ||
         (!m.getSelectedColor().equals(TextModel.DEF_SELECTED_COLOR))) {
       template = tm.loadTemplate(COLOR_TEMPLATE);
       outputLines = tm.expandMacros(template, map);
       tm.codeWriter(sBd, outputLines);
     }
-    
+
     if (m.isUTF8() && Controller.getTargetPlatform().equals(ProjectModel.PLATFORM_LINUX)) {
       template = tm.loadTemplate(TEXTUTF8_TEMPLATE);
       outputLines = tm.expandMacros(template, map);
@@ -163,7 +171,7 @@ public final class TextCodeBlock implements CodeBlock {
     template.clear();
     outputLines.clear();
     map.clear();
-    return sBd;   
+    return sBd;
   }
-  
+
 }
